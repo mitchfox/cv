57bad2693cf9cf8c4d8836d7242ae1c62a24937a
mitchfox <mjfox99@gmail.com>
2024-07-16 22:43:26 +0100

    added line

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -48,6 +48,7 @@ export default function Header() {
 
       </div>
 
+
         <a
           className="btn-sm text-gray-200 dark:text-gray-800 bg-gradient-to-r from-gray-800 to-gray-700 dark:from-gray-300 dark:to-gray-100 dark:hover:bg-gray-100 shadow relative before:absolute before:inset-0 before:rounded-[inherit] before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white/.2)_50%,transparent_75%,transparent_100%)] dark:before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white)_50%,transparent_75%,transparent_100%)] before:bg-[length:250%_250%,100%_100%] before:bg-[position:200%_0,0_0] before:bg-no-repeat before:[transition:background-position_0s_ease] hover:before:bg-[position:-100%_0,0_0] hover:before:duration-[1500ms]"
           href='mailto:xyz@yourapplicationdomain.com?subject=Me&body=Hello!'
